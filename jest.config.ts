ba1a93f6f9e8f13ac732a7e82c62bd5bb2a193b1
codevil <namhundred@naver.com>
2022-10-26 15:29:02 +0900

    config:jest

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	testEnvironment: 'jsdom',
-	transform: {
-    '\\.js$': 'babel-jest',
-  },
-	testPathIgnorePatterns: ['/node_modules/'],
-	setupFilesAfterEnv: ['./jest.setup.js'],
-}
--- public/utils/getRandom.test.js
+++ public/utils/getRandom.test.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import {getRandom} from "./getRandom.js";
+const {getRandom} = require('./getRandom')
 
 test('getRandom(10)의 반환 값은 0보다 크거나 같고, 10보다 작거나 같다.', () =>{
-    //기대 값  expect
-    //전달 값 received
-    expect(getRandom(10)).toBeLessThenOrEqual()
+    let result = getRandom(10)
+    let value = 10
+    expect(result).toBeLessThenOrEqual(value)
+    expect(result).toBeLessThenOrEqual(value)
 })
-=======
-import { getRandom } from './getRandom.js';
-
-test('getRandom(10)의 반환 값은 0보다 크거나 같고, 10보다 작거나 같다.', () => {
-  expect(getRandom(10)).toBeLessThanOrEqual(10);
-});
->>>>>>> 4c3c68d894b82da71de12b2722cd841cba7aeb6d
