47d7fb5b9ffe67be6f1296bbd0d81b162cb3a5c4
codevil <namhundred@naver.com>
2022-10-26 14:49:27 +0900

    merge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,10 @@
+import 'core-js';
 ((global) => {
   'use strict';
+  // 브라우저 (사용자 에이전트) 정보
+  const {userAgent} = navigator;
+
+  //IOS 운영체제 여부 확인 유틸리티 함수
 
   console.log(global.navigator.userAgent);
 })(globalThis);
