2de5f5cfe010903f9d4fab64821b64cd071e4554
codevil <namhundred@naver.com>
2022-10-26 16:10:10 +0900

    merge

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: [['@babel/preset-env', { useBuiltIns: 'usage', corejs: '3.26.0', modules:false, }]],
+  presets: [['@babel/preset-env', { 
+    // useBuiltIns: 'usage', corejs: '3.26.0', modules:false, 
+  }]],
 };
