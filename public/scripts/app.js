460b75466ec604c0fcca65f2e3c2f60db4a56df1
codevil <namhundred@naver.com>
2022-10-27 15:37:26 +0900

    merge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'core-js';
 ((global) => {
   'use strict';
@@ -6,9 +5,6 @@ import 'core-js';
   const {userAgent} = navigator;
 
   //IOS 운영체제 여부 확인 유틸리티 함수
-=======
-import * as Utils from '../utils/index.js';
->>>>>>> f458ae1befce1a23a0c2d9f06a33ab3999217f7c
 
 let price = 984_000;
 
