261b3749c724577e24e4d7741c1d4554405b0e8e
codevil <namhundred@naver.com>
2022-10-27 16:34:14 +0900

    merge

--- public/utils/fetchBooks.js
+++ public/utils/fetchBooks.js
@@ -1,19 +1,44 @@
 // `/api/books.json` 모듈을 불러와 booksApi 이름으로 사용합니다.
 // import('../api/books.json', { assert: { type: 'json' } });
-import booksApi from '../api/books.json' assert { type: 'json' };
+// import booksApi from '../api/books.json' assert { type: 'json' };
+import booksApi from '../api/books.json'
+// import { fetchBooks } from './fetchBooks';
 
-export const fetchBooks = (timeout = 1000, shouldReject = false) => {
-  // Promise 객체를 생성 반환합니다.
-  return new Promise((resolve, reject) => {
-    // timeout 초가 흐른 뒤, Promise.resolve, Promise.reject가 작동하도록 설정합니다.
-    setTimeout(() => {
-      // shouldReject 값에 따라 오류 객체를 전송하거나,
-      if (!shouldReject) {
-        // booksApi.data 값을 전송합니다.
-        resolve(booksApi.data);
-      } else {
-        reject({ message: 'UNKNOWN ERROR' });
-      }
-    }, timeout);
-  });
-};
+//1. XMLHttpRequest
+//2. fetch API
+export const fetchBooks = (timeout = 1000, shouldReject = false) =>{
+  const xhr = new XMLHttpRequest();
+  const method = "GET";
+  const url = "../api/books.json";
+  xhr.open(method, url);
+
+return new Promise((resolve, reject) => {
+  // timeout 초가 흐른 뒤, Promise.resolve, Promise.reject가 작동하도록 설정합니다.
+  setTimeout(() => {
+    // shouldReject 값에 따라 오류 객체를 전송하거나,
+    if (!shouldReject) {
+      // booksApi.data 값을 전송합니다.
+      resolve(booksApi.data);
+    } else {
+      reject({ message: 'UNKNOWN ERROR' });
+    }
+  }, timeout);
+});
+
+xhr.send();
+}
+// export const fetchBooks = (timeout = 1000, shouldReject = false) => {
+//   // Promise 객체를 생성 반환합니다.
+//   return new Promise((resolve, reject) => {
+//     // timeout 초가 흐른 뒤, Promise.resolve, Promise.reject가 작동하도록 설정합니다.
+//     setTimeout(() => {
+//       // shouldReject 값에 따라 오류 객체를 전송하거나,
+//       if (!shouldReject) {
+//         // booksApi.data 값을 전송합니다.
+//         resolve(booksApi.data);
+//       } else {
+//         reject({ message: 'UNKNOWN ERROR' });
+//       }
+//     }, timeout);
+//   });
+// };
