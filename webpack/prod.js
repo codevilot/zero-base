08920d69297c55d12a63b9c976ff4b23cd1a4484
codevil <namhundred@naver.com>
2022-11-04 15:43:35 +0900

    pull

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,13 +1,17 @@
 import { resolve } from 'node:path';
 import { merge } from 'webpack-merge';
 import commonConfig from './common.js';
-
+import { createCopyPlugin } from './plugins/index.js';
 const prodConfig = merge(commonConfig, {
   mode: 'production',
   output: {
     ...commonConfig.output,
     path: resolve('build'),
   },
+  plugins: [
+    ...commonConfig.plugins,
+    createCopyPlugin(),
+  ].filter(Boolean),
 });
 
 export default prodConfig;
