22b4d2bdf65dbc90ad189019438d47546150240b
codevil <namhundred@naver.com>
2022-11-04 14:18:13 +0900

    merge it

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-import 'regenerator-runtime';
-import '@testing-library/jest-dom';
-=======
 import '@testing-library/jest-dom/extend-expect';
->>>>>>> 4bbfc1f6b689ba93b4ba3dfdb2cead57c0eb0287
